ffebc72762f3a646b68df35fa1bc0be096ffe0a5
tizz98 <dev.tizz98@gmail.com>
2014-10-28 20:25:26 -0400

    using cloudfront again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_controller.asset_host = ENV['CDN_BASE']
-  # config.font_assets.origin = ENV['FONT_BASE']
+  config.action_controller.asset_host = ENV['CDN_BASE']
+  config.font_assets.origin = ENV['FONT_BASE']
 end
