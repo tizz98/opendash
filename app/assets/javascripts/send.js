0a0f83c8d1c8f9f9639f64860d841f23eb494011
tizz98 <dev.tizz98@gmail.com>
2014-10-30 19:02:01 -0400

    removed unnecessary function from send.js

--- app/assets/javascripts/send.js
+++ app/assets/javascripts/send.js
@@ -8,12 +8,3 @@ function sendData(data) {
 	  data: data,
 	});
 }
-
-function changeLinks(url) {
-	var menu_url = document.getElementById('menu_url');
-	var modal_url = document.getElementById('modal_url');
-
-	menu_url.innerHTML = url;
-
-	modal_url.innerHTML = '<a href="/d/' + url + '">Copy me</a>';
-}
