4a9ba384259a6e092252036047c85114660c93d5
tizz98 <dev.tizz98@gmail.com>
2014-11-07 19:26:33 -0500

    added portlets.js docs

--- app/assets/javascripts/portlets.js
+++ app/assets/javascripts/portlets.js
@@ -1,14 +1,28 @@
+/*
+	This is a fun js file, it uses jQuery UI to let us
+	make the different modules sortable as well as the
+	stocks sortable. We need two different columns here because
+	we have different settings for each one
+*/
+
+// Modules sortable portlets
+// we don't have the placeholder attribute
+// because we create our own via CSS which
+// 
 $( ".column" ).sortable({
       connectWith: ".column",
       handle: ".portlet-content",
-    });
- 
+});
+
+// globally add these classes to the portlet elements
+// this is the same for both columns
 $( ".portlet" )
   .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" );
 
 
+// Stocks sortable portlets
 $( ".column2" ).sortable({
       connectWith: ".column2",
       handle: ".portlet-content",
       placeholder: "portlet-placeholder ui-corner-all"
-    });
+});
